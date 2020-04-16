b6b0813cb38af3e0c742db9de5e7ee14ebd6dc76
Angel Magalhães <angelsanmag@gmail.com>
2020-04-16 11:44:19 +0100

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -15,7 +15,8 @@ namespace SalesWebMvc.Controllers
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MCV App from C# Course";
+            ViewData["Student"] = "Angel Magalhães";
 
             return View();
         }
