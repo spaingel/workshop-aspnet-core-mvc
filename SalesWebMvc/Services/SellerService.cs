26ef5ea7365f0d800135bb98932daac37b9d42f2
Angel Magalhães <angelsanmag@gmail.com>
2020-04-20 11:36:33 +0100

    Simple Create form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -17,5 +17,10 @@ namespace SalesWebMvc.Services
         public List<Seller> FindAll() {
             return _context.Sellers.ToList();
         }
+
+        public void Insert(Seller obj) {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
